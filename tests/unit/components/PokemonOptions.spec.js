00712993b83f603f7261b96982300a53f6744637
JaViKo <javikogutierrez64@gmail.com>
2023-04-06 00:34:08 -0500

    check snapshot"

---
+++ tests/unit/components/PokemonOptions.spec.js
@@ -0,0 +1,19 @@
+import { shallowMount } from '@vue/test-utils'
+import  PokemonOptions  from '@/components/PokemonOptions'
+
+
+import { pokemons } from '../mocks/pokemons.mock'
+
+describe('PokemonOptions Component', () => {
+    let wrapper
+    beforeEach( () => {
+        wrapper = shallowMount( PokemonOptions, {
+            props: {
+                pokemons
+            }
+        })
+    } );
+    test( 'do match snapshot ', () => {
+        expect( wrapper.html() ).toMatchSnapshot()
+    })
+});
---
+++ tests/unit/mocks/pokemons.mock.js
@@ -0,0 +1,6 @@
+export const pokemons = [
+    { name: 'bulbasaur', id: 1 },
+    { name: 'venusaur', id: 3 },
+    { name: 'charmander', id: 4 },
+    { name: 'ivysaur', id: 2 },
+];
