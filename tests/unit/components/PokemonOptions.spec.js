cf153db0eb82f607746a48ef9e76e10e1d8a0694
JaViKo <javikogutierrez64@gmail.com>
2023-04-09 19:30:37 -0500

    test li tags and have pokemons names

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -16,4 +16,16 @@ describe('PokemonOptions Component', () => {
     test( 'do match snapshot ', () => {
         expect( wrapper.html() ).toMatchSnapshot()
     })
+    test('Show 4 options correctly', () => {
+        const liPokemons = wrapper.findAll('li');
+        expect( liPokemons ).toHaveLength(4);
+        const namesLi = liPokemons.map((li) => li.text());
+        const pokemonsNames = pokemons.map(({ name }) => name);
+        expect(namesLi).toEqual(pokemonsNames);
+        
+    });
+    test('should emit "selection" whit parameter on do click', () => {
+
+    });
+
 });
