c76f7077c62bf10ec16e8bb81fbb6828f8c06dcf
JaViKo <javikogutierrez64@gmail.com>
2023-04-09 20:48:53 -0500

    test in propeties reactives

--- tests/unit/pages/PokemonPage.spec.js
+++ tests/unit/pages/PokemonPage.spec.js
@@ -58,4 +58,27 @@ describe('PokemonPage component', () => {
         expect( pokemonPicture.attributes('pokemonid') ).toBe('1');
         expect( pokemonOptions.attributes('pokemons') ).toBeTruthy();
     });
+
+
+    test( 'test in checkAnswer', async () => {
+        const wrapper = shallowMount( PokemonPage, {
+            data() {
+                return {
+                    pokemonsArr: pokemons,
+                    pokemon: pokemons[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: ''
+                }
+            }
+        } );
+
+        await wrapper.vm.checkAnswer(1);
+        expect( wrapper.find('h2').exists() ).toBeTruthy();
+        expect( wrapper.vm.showPokemon ).toBeTruthy();
+        expect( wrapper.find('h2').text() ).toBe(`Correct, ${pokemons[0].name}`);
+
+        await wrapper.vm.checkAnswer(10);
+        expect( wrapper.vm.message ).toBe(`Oops, is ${pokemons[0].name}`);
+    });
 });
