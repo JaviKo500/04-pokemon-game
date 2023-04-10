8ebdad8ae8e24099a286ecba0cb7fc04ff786017
JaViKo <javikogutierrez64@gmail.com>
2023-04-09 19:40:43 -0500

    test with emites

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -25,6 +25,20 @@ describe('PokemonOptions Component', () => {
         
     });
     test('should emit "selection" whit parameter on do click', () => {
+        const [li1, li2, li3, li4] = wrapper.findAll('li');
+
+        li1.trigger('click');
+        li2.trigger('click');
+        li3.trigger('click');
+        li4.trigger('click');
+        
+        expect( wrapper.emitted('selection') ).toHaveLength(4);
+
+        expect( wrapper.emitted('selection')[0] ).toEqual([1]);
+        expect( wrapper.emitted('selection')[1] ).toEqual([3]);
+        expect( wrapper.emitted('selection')[2] ).toEqual([4]);
+        expect( wrapper.emitted('selection')[3] ).toEqual([2]);
+
 
     });
 
